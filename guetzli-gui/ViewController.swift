3836c4a97012b0f49f44d7881a3d11755299c7f4
Ivan Cristina <ivancristina5@gmail.com>
2018-10-29 13:09:26 +0100

    New tabbed view with recurisvely compression

--- guetzli-gui/ViewController.swift
+++ guetzli-gui/ViewController.swift
@@ -17,15 +17,25 @@ class ViewController: NSViewController {
     @IBOutlet weak var compressButton: NSButton!
     @IBOutlet weak var warningField: NSTextField!
     
+    @IBOutlet weak var inFolderUrlField: NSTextField!
+    @IBOutlet weak var qualityFolderField: NSTextField!
+    @IBOutlet weak var progressFolderIndicator: NSProgressIndicator!
+    @IBOutlet weak var compressFolderButton: NSButton!
+    @IBOutlet weak var installRecursivelyButton: NSButton!
     
     var inFile : URL?
     var outFile : URL?
     var quality = 85
     
+    var inFolder : URL?
+    var outFolder : URL?
+    var qualityFolder = 85
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         qualityField.integerValue = quality
+        qualityFolderField.integerValue = qualityFolder
         
         // Do any additional setup after loading the view.
     }
@@ -46,6 +56,24 @@ class ViewController: NSViewController {
             self.inFileUrlField.stringValue = openPanel.urls[0].path
         }
     }
+    
+    @IBAction func onOpenFolder(_ sender: AnyObject) {
+        let openPanel = NSOpenPanel();
+        
+        openPanel.allowsMultipleSelection = false
+        openPanel.canChooseDirectories = true
+        openPanel.canCreateDirectories = true
+        openPanel.canChooseFiles = false
+        
+        openPanel.begin { (result) in
+            guard result == .OK else { return }
+            
+            self.inFolder = openPanel.urls[0]
+            self.inFolderUrlField.stringValue = openPanel.urls[0].path
+        }
+        
+    }
+        
     
     @IBAction func onSaveFile(_ sender: AnyObject) {
         let savePanel = NSSavePanel()
@@ -71,6 +99,14 @@ class ViewController: NSViewController {
         quality = sender.integerValue
     }
     
+    @IBAction func qualityFolderDidChange(_ sender: NSTextField) {
+        if sender.integerValue < 84 {
+            sender.integerValue = 84
+        }
+        
+        qualityFolder = sender.integerValue
+    }
+    
     @IBAction func sliderDidChange(_ sender: NSSlider) {
         qualityField.integerValue = sender.integerValue
         quality = sender.integerValue
@@ -82,6 +118,15 @@ class ViewController: NSViewController {
         if sender.integerValue > 84 {
             warningField.isHidden = true
         }
+    }
+    
+    @IBAction func sliderFolderDidChange(_ sender: NSSlider) {
+        qualityFolderField.integerValue = sender.integerValue
+        qualityFolder = sender.integerValue
+    }
+    
+    @IBAction func installRecursively(_ sender: NSButton) {
+        NSWorkspace.shared.open(NSURL(string: "https://github.com/tanrax/guetzli-recursively/blob/master/README.md")! as URL)
     }
     
     @IBAction func compress(_ sender: NSButton) {
@@ -112,6 +157,30 @@ class ViewController: NSViewController {
             })
           },
           binUrl.path, "--quality", "\(quality)", inFilePath, outFilePath)
+    }
+    
+    @IBAction func compressFolder(_ sender: NSButton) {
+        guard let inFolderPath = inFolder?.path else {
+            return
+        }
+        
+        qualityFolderField.isEnabled = false
+        progressFolderIndicator.isHidden = false
+        progressFolderIndicator.startAnimation(nil)
+        compressFolderButton.isEnabled = false
+        compressFolderButton.title = "Compressing..."
+        
+        Command.execAsync(terminationHandler: { (task) in
+            DispatchQueue.main.async(execute: {
+                self.progressFolderIndicator.isHidden = true
+                self.progressFolderIndicator.stopAnimation(nil)
+                self.compressFolderButton.isEnabled = true
+                self.compressFolderButton.title = "Compress"
+                
+                self.qualityFolderField.isEnabled = true
+            })
+        },
+                          "guetzli_recursively --quality", "\(qualityFolder)", inFolderPath)
     }
 }
 
